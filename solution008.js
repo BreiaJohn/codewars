c825f00482079474bc195a920cedcab2201ecb85
Breia John <breiamj4@gmail.com>
2022-05-25 21:01:28 -0400

    solution008

---
+++ solution008.js
@@ -0,0 +1,15 @@
+
+function bmi(weight, height) {
+    let bmi = weight / (height**2);
+    
+    if(bmi < 18.5){
+      return "Underweight";
+    }else if (bmi < 25){
+      return "Normal";
+    }else if (bmi < 30){
+      return "Overweight";
+    }else{
+      return "Obese";
+    }
+  }
+  
