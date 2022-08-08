dba311f15babba0addf08cd93419804f80608614
Breia John <breiajohn@Breias-MacBook-Pro.local>
2022-08-08 17:55:07 -0400

    update codewars

---
+++ solution010.js
@@ -0,0 +1,12 @@
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+    }
+    
