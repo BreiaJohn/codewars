450af1351a3c4e384e3dd3ef0ec7a737c85f9457
Breia John <breiajohn@Breias-MacBook-Pro.local>
2022-08-09 16:53:29 -0400

    codewars update 11

---
+++ solution011.js
@@ -0,0 +1,12 @@
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+function enough(cap, on, wait) {
+    // your code here
+  return Math.max(wait + on -cap, 0)
+  
+  }
