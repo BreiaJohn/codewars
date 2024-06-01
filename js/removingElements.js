e6956aea9ff33cc780e7bb2d786b8b94063053ad
Breia <breiamj4@gmail.com>
2024-06-01 13:43:17 -0400

    another way to remove elements

--- js/removingElements.js
+++ js/removingElements.js
@@ -17,3 +17,10 @@ function removeEveryOther(arr){
  }
  return z;
   }
+
+//   Take an array and remove every second element from the array. 
+//   Always keep the first element and start removing with the next element.
+
+function removeEveryOther(arr){
+    return arr.filter((_, index)=> index % 2 == 0);
+}
