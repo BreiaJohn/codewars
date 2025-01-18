726b86492844e481378a7a8da7bae50b8b1da102
Breia <breiamj4@gmail.com>
2025-01-18 18:37:25 -0500

    Create cockroach.js
    coding challenge

---
+++ js/cockroach.js
@@ -0,0 +1,12 @@
+// The cockroach is one of the fastest insects.
+//  Write a function which takes its speed in km
+//  per hour and returns it in cm per second, 
+// rounded down to the integer (= floored).
+
+
+
+function cockroachSpeed(s) {
+    //Good Luck!
+   return Math.floor(s * 27.7778)
+   
+  }
